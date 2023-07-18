e08fc299ae70334f716b402ad580f6094cb84d17
nsdhl <dhlnischal10@gmail.com>
2023-07-18 18:30:21 +0545

    feat:created routes for differe

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import Contact from "./routes/Contact";
+import Home from "./routes/Home";
+import { Routes, Route } from "react-router-dom";
+
+
+const App = () =>{
+  return (
+   <>
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/project" element={<Project />} />
+    <Route path="/contact" element={<Contact />} />
+   </Routes>
+   </>
+  )
+}
+
+export default App;
