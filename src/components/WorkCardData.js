bb0282f140d686a8bf7a609e6b29cd9c35673f85
nsdhl <dhlnischal10@gmail.com>
2023-07-25 16:31:02 +0545

    fixed the issue of product card

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -6,22 +6,21 @@ import introbg from '../assets/intro-bg.jpg';
 const projectCardData   = [
     {
 imgsrc: project1,
-title: "Project Title",
-text: "This is a text",
+title: "ResQ",
+text: "ResQ is a Progressive Web App that allows users to report incident of the disasters in their area and also to ask for the urgent assistance",
 view: "https://youtu.be/q29MxRthrVM?list=RDMMgvh1vjkxhoA"
     },
 
     {
         imgsrc: pro1,
-        title: "Project Title",
-        text: "This is a text",
+        title: "Cheers",text: "Cheers is Web Application that allows users to buy the liquor products from different categories",
         view: "https://youtu.be/q29MxRthrVM?list=RDMMgvh1vjkxhoA"
             },
 
             {
                 imgsrc: introbg,
-                title: "Project Title",
-                text: "This is a text",
+                title: "Kisan Area",
+                text: "Kisan Area is a Web Application that allows users to buy the agriculture products from different categories",
                 view: "https://youtu.be/q29MxRthrVM?list=RDMMgvh1vjkxhoA"
 
                     },
